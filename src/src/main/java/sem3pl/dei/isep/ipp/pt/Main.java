650a1a510476a162b6387db6b485c95e1dd88140
Miguel <1211281@isep.ipp.pt>
2023-10-25 23:36:03 +0100

    controller reading and creating sql file successfuly

--- src/src/main/java/sem3pl/dei/isep/ipp/pt/Main.java
+++ src/src/main/java/sem3pl/dei/isep/ipp/pt/Main.java
@@ -1,27 +1,12 @@
 package sem3pl.dei.isep.ipp.pt;
 
+import sem3pl.dei.isep.ipp.pt.lapr3.application.controller.ImportLegacyController;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        /*
-        ImportLegacyController importLegacyController = new ImportLegacyController();
-
-
-        for (String str :importLegacyController.readFile("Legacy_Data.xlsx",0)) {
-            System.out.println(str);
-        }
-        for (String str :importLegacyController.readFile("Legacy_Data.xlsx",1)) {
-            System.out.println(str);
-        }
-        for (String str :importLegacyController.readFile("Legacy_Data.xlsx",2)) {
-            System.out.println(str);
-        }
-        for (String str :importLegacyController.readFile("Legacy_Data.xlsx",3)) {
-            System.out.println(str);
-        }
-        */
-
-
-
 
             ApplicationChooser applicationChooser = new ApplicationChooser();
         applicationChooser.run();
