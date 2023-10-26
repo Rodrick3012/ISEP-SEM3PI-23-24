995b4eed39dc835135eb5dd2dc6d155087b1fb19
Miguel <1211281@isep.ipp.pt>
2023-10-27 00:42:00 +0100

    update scrip from usbd05 to show the product name

--- docs/Sprint 1/BDDAD/USBD05/usbd05.sql
+++ docs/Sprint 1/BDDAD/USBD05/usbd05.sql
@@ -1,8 +1,9 @@
-select parcela,plantacao.nomecomum,plantacao.variedade,count(*) as quantidade
+select parcela,plantacao.nomecomum,plantacao.variedade,plantacao.produto,sum(quantidade) as quantidade , unidade.unidade
 from operacao
 inner join plantacao on operacao.plantacao=plantacao.id
 inner join tipooperacao on tipooperacao.id=operacao.tipooperacao
+inner join unidade on operacao.unidade=unidade.id
 where parcela like 'Lameiro da ponte'
     and tipooperacao.tipooperacao like 'Colheita'
-    and data between '7-jan-2022' and '18-oct-2022'
-group by parcela,plantacao.nomecomum,plantacao.variedade;
+    and data between '7-jan-2021' and '18-oct-2022'
+group by parcela,plantacao.nomecomum,plantacao.variedade,plantacao.produto,unidade.unidade;
