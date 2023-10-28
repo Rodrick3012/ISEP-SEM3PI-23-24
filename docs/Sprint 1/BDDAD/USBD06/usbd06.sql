56b650b3c22363d6a0531da438ccdb6d21d72ce3
rodri <1220636@isep.ipp.pt>
2023-10-28 16:26:51 +0100

    querie us06 and us07

--- docs/Sprint 1/BDDAD/USBD06/usbd06.sql
+++ docs/Sprint 1/BDDAD/USBD06/usbd06.sql
@@ -1,6 +1,9 @@
-select parcela,classificacao.classificacao,count (fatorproducao) as aplicações
-from operacaofatorproducao
-inner join fatorproducao on operacaofatorproducao.fatorproducao=fatorproducao.designacao
-inner join classificacao on  classificacao.id=fatorproducao.classificacao
-where data between '02-oct-2010' and '02-may-2021'
-group by parcela,classificacao.classificacao;
+
+SELECT operacaoFatorProducao.parcela, classificacao.classificacao, COUNT(operacaoFatorProducao.parcela) AS contagemFatoresProducao
+FROM operacaoFatorProducao
+         INNER JOIN parcela ON parcela.designacao = operacaoFatorProducao.parcela
+         INNER JOIN fatorProducao ON fatorProducao.designacao = operacaoFatorProducao.fatorProducao
+         INNER JOIN classificacao ON classificacao.id = fatorProducao.classificacao
+WHERE operacaoFatorProducao.parcela LIKE 'Vinha'
+  AND operacaoFatorProducao.data BETWEEN TO_DATE('2021-01-07', 'YYYY-MM-DD') AND TO_DATE('2021-10-18', 'YYYY-MM-DD')
+GROUP BY operacaoFatorProducao.parcela, classificacao.classificacao;
