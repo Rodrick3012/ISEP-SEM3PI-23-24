4d266bed7b4ee9ff739e984ee99db640bde03d46
rodri <1220636@isep.ipp.pt>
2023-10-28 18:20:37 +0100

    querie US08 problem fixed

--- docs/Sprint 1/BDDAD/USBD08/usbd08.sql
+++ docs/Sprint 1/BDDAD/USBD08/usbd08.sql
@@ -1,5 +1,18 @@
-SELECT operacaoFatorProducao.fatorProducao, count (operacaoFatorProducao.fatorProducao)
-FROM operacaoFatorProducao
-INNER JOIN fatorProducao ON fatorProducao.designacao = operacaoFatorProducao.fatorProducao
-WHERE operacaoFatorProducao.data BETWEEN TO_DATE('2022-12-11', 'YYYY-MM-DD') AND TO_DATE('2023-03-20', 'YYYY-MM-DD')
-GROUP BY operacaoFatorProducao.fatorProducao
+SELECT fatorProducao, total_aplicacoes
+FROM (
+         SELECT operacaoFatorProducao.fatorProducao, COUNT(operacaoFatorProducao.fatorProducao) AS total_aplicacoes
+         FROM operacaoFatorProducao
+            INNER JOIN fatorProducao ON fatorProducao.designacao = operacaoFatorProducao.fatorProducao
+         WHERE operacaoFatorProducao.data BETWEEN TO_DATE('2009-12-11', 'YYYY-MM-DD') AND TO_DATE('2023-03-20', 'YYYY-MM-DD')
+         GROUP BY operacaoFatorProducao.fatorProducao
+     )
+WHERE total_aplicacoes = (
+    SELECT MAX(total_aplicacoes)
+    FROM (
+             SELECT operacaoFatorProducao.fatorProducao, COUNT(operacaoFatorProducao.fatorProducao) AS total_aplicacoes
+             FROM operacaoFatorProducao
+                INNER JOIN fatorProducao ON fatorProducao.designacao = operacaoFatorProducao.fatorProducao
+             WHERE operacaoFatorProducao.data BETWEEN TO_DATE('2009-12-11', 'YYYY-MM-DD') AND TO_DATE('2023-03-20', 'YYYY-MM-DD')
+             GROUP BY operacaoFatorProducao.fatorProducao
+         )
+);
