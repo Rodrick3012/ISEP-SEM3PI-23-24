d2183e2ac37e28c04d9ed776fb49099b7dc9e452
Miguel <1211281@isep.ipp.pt>
2023-10-29 21:45:18 +0000

    usbd09 done

--- docs/Sprint 1/BDDAD/USBD09/usbd09.sql
+++ docs/Sprint 1/BDDAD/USBD09/usbd09.sql
@@ -1,4 +1,6 @@
-SELECT fatorproducao,count(*) AS quantidade
+SELECT classificacao.classificacao,count(*) AS quantidade
 FROM operacaofatorproducao
+inner join fatorproducao on operacaofatorproducao.fatorproducao=fatorproducao.designacao
+inner join classificacao on classificacao.id=fatorproducao.classificacao
 WHERE data BETWEEN '01-jan-2010' AND '01-jan-2023'
-GROUP BY fatorproducao;
+GROUP BY classificacao.classificacao;
