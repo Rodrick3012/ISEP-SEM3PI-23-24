503cea9488cd74aeb9375c3a5a0dc76d26e9d48e
Mario_Joao <1221019@isep.ipp.pt>
2023-10-29 19:59:12 +0000

    us10 BDDAD

--- docs/Sprint 1/BDDAD/USBD10/usbd10.sql
+++ docs/Sprint 1/BDDAD/USBD10/usbd10.sql
@@ -9,4 +9,5 @@ having count(*)= (select max(count(*))
 				where lower(tipooperacao.tipooperacao) like lower('Rega')and
     			data between '01-jan-2010' AND '01-jan-2023'
     			group by parcela)
+
 group by parcela;
