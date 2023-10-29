506b980380593c954eaaae30adcfce9df4ce9215
Miguel <1211281@isep.ipp.pt>
2023-10-29 20:38:16 +0000

    change usbd07.sql showing in type correctly

--- docs/Sprint 1/BDDAD/USBD07/usbd07.sql
+++ docs/Sprint 1/BDDAD/USBD07/usbd07.sql
@@ -8,12 +8,13 @@ GROUP BY operacao.parcela, tipoOperacao.tipoOperacao
 
 UNION
 
-SELECT operacaoFatorProducao.parcela, classificacao.classificacao, COUNT(operacaoFatorProducao.parcela) AS contagemFatoresProducao
+SELECT operacaoFatorProducao.parcela, tipooperacao.tipooperacao, COUNT(operacaoFatorProducao.parcela) AS contagemFatoresProducao
 FROM operacaoFatorProducao
          INNER JOIN parcela ON parcela.designacao = operacaoFatorProducao.parcela
          INNER JOIN fatorProducao ON fatorProducao.designacao = operacaoFatorProducao.fatorProducao
-         INNER JOIN classificacao ON classificacao.id = fatorProducao.classificacao
+         INNER JOIN tipoOperacao on tipoOperacao.id = operacaoFatorProducao.tipoOperacao
 WHERE operacaoFatorProducao.parcela LIKE 'Vinha'
   AND operacaofatorproducao.data BETWEEN '11-dec-2022' AND '20-mar-2023'
-GROUP BY operacaoFatorProducao.parcela, classificacao.classificacao;
+GROUP BY operacaoFatorProducao.parcela, tipooperacao.tipooperacao;
+
 
