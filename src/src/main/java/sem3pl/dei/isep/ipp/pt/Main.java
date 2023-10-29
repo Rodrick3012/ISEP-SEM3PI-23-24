adf961cb07ff95f93ed9a9b063430a17eb6731e4
Miguel <1211281@isep.ipp.pt>
2023-10-29 12:50:30 +0000

    add option to create de sql for legacy file and show the data correctly

--- src/src/main/java/sem3pl/dei/isep/ipp/pt/Main.java
+++ src/src/main/java/sem3pl/dei/isep/ipp/pt/Main.java
@@ -4,9 +4,7 @@ import sem3pl.dei.isep.ipp.pt.bddad.ImportLegacyController;
 
 public class Main {
     public static void main(String[] args) {
-        /*ImportLegacyController importLegacyController = new ImportLegacyController();
-        importLegacyController.writeSQLToFile(importLegacyController.readFile("Legacy_Data.xlsx"),"docs/Sprint 1/BDDAD/USBD04/usbd04.sql");*/
-            ApplicationChooser applicationChooser = new ApplicationChooser();
+        ApplicationChooser applicationChooser = new ApplicationChooser();
         applicationChooser.run();
     }
 }
