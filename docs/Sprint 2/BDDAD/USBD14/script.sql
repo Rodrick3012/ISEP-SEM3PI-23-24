952f781ae83537f93937910595bd754120ff29c7
rodri <1220636@isep.ipp.pt>
2023-11-18 14:01:02 +0000

    US 11 e 14 base de dados

--- docs/Sprint 2/BDDAD/USBD14/script.sql
+++ docs/Sprint 2/BDDAD/USBD14/script.sql
@@ -2,121 +2,86 @@
 
 --Parametros - data, quantidade, parcela, modo, fatorProducao, cultura, tipoOperacao
 
---Calda Bordalesa ASCENZA
-
-CREATE OR REPLACE PROCEDURE inserirOperacaoAplicacaoFatorProducao(data1 operacaoFatorProducao.data%type
-    ,quantidade1 operacaoFatorProducao.quantidade%type
-    ,parcela1 operacaoFatorProducao.parcela%type
-    ,fp operacaoFatorProducao.fatorProducao%type
-    ,cultura1 operacaoFatorProducao.cultura%type
-    ,tipoOp operacaoFatorProducao.tipoOperacao%type)
+CREATE OR REPLACE PROCEDURE inserirOperacaoAplicacaoFatorProducao(
+    data1 operacaoFatorProducao.data%type,
+    quantidade1 operacaoFatorProducao.quantidade%type,
+    parcela1 operacaoFatorProducao.parcela%type,
+    fp operacaoFatorProducao.fatorProducao%type,
+    cultura1 operacaoFatorProducao.cultura%type,
+    tipoOp operacaoFatorProducao.tipoOperacao%type
+)
 IS
-
-var_boolean boolean;
-dataInvalida EXCEPTION;
-quantidadeInvalida EXCEPTION;
-culturaParcelaInvalida EXCEPTION;
-fpInvalido EXCEPTION;
-toInvalido EXCEPTION;
-
+    var_boolean BOOLEAN;
 BEGIN
     var_boolean := verificarDataNaoEstaNoFuturo(data1);
+    IF var_boolean THEN
+        DBMS_OUTPUT.PUT_LINE('Data Válida!!! ' );
+END IF;
 
-CASE
-    WHEN var_boolean THEN
-      DBMS_OUTPUT.PUT_LINE('Data Válida!!! ' );
-ELSE
-	  RAISE dataInvalida;
-END CASE;
+    var_boolean := verificarQuantidadeValida(quantidade1, cultura1);
+    IF var_boolean THEN
+        DBMS_OUTPUT.PUT_LINE('Quantidade Válida!!! ' );
+END IF;
 
-	var_boolean := verificarQuantidadeValida(quantidade1, cultura1);
-CASE
-	WHEN var_boolean THEN
-      DBMS_OUTPUT.PUT_LINE('Quantidade Válida!!! ' );
-ELSE
-	  RAISE quantidadeInvalida;
-END CASE;
+    var_boolean := validateCulturaNaParcela(parcela1, cultura1);
+    IF var_boolean THEN
+        DBMS_OUTPUT.PUT_LINE('Cultura e parcela Válidos!!!');
+END IF;
 
-	var_boolean := validateCulturaNaParcela(parcela1,cultura1);
-CASE
-	WHEN var_boolean THEN
-      DBMS_OUTPUT.PUT_LINE('Cultura e parcela Válidos!!!');
-ELSE
-    RAISE culturaParcelaInvalida;
-END CASE;
+    var_boolean := validateFatorProducao(fp);
+    IF var_boolean THEN
+        DBMS_OUTPUT.PUT_LINE('Fator de Produção válido!!!');
+END IF;
 
-	var_boolean := validateFatorProducao(fp);
-CASE
-    WHEN var_boolean THEN
-      DBMS_OUTPUT.PUT_LINE('fator de Producao válido!!!');
-ELSE
-    RAISE fpInvalido;
-END CASE;
+    var_boolean := validateOperacao(tipoOp);
+    IF var_boolean THEN
+        DBMS_OUTPUT.PUT_LINE('Tipo de Operação válido!!!');
+END IF;
 
-	var_boolean := validateOperacao(tipoOp);
-CASE
-    WHEN var_boolean THEN
- 	  DBMS_OUTPUT.PUT_LINE('tipo operação válido!!!');
-ELSE
-    RAISE toInvalido;
-END CASE;
-insert into operacaoFatorProducao(data,quantidade,parcela,modo,fatorproducao,cultura ,tipooperacao) values(data1,quantidade1,parcela1,2,fp,cultura1,tipoOp);
+INSERT INTO operacaoFatorProducao(data, quantidade, parcela, modo, fatorproducao, cultura, tipooperacao)
+VALUES (data1, quantidade1, parcela1, 2, fp, cultura1, tipoOp);
 
 DBMS_OUTPUT.PUT_LINE('Inserção bem-sucedida: ' || SQL%ROWCOUNT || ' linha(s) inserida(s).');
 
 EXCEPTION
-    WHEN culturaParcelaInvalida THEN
-        RAISE_APPLICATION_ERROR(-20001, 'insert fail cultura/parcela inválidos');
-WHEN dataInvalida THEN
-  		RAISE_APPLICATION_ERROR(-20001, 'insert fail data inválida');
-WHEN quantidadeInvalida THEN
-        RAISE_APPLICATION_ERROR(-20001, 'insert fail quantidade inválida');
-WHEN fpInvalido THEN
-        RAISE_APPLICATION_ERROR(-20001, 'insert fail fator producao inválido');
-WHEN toInvalido THEN
-        RAISE_APPLICATION_ERROR(-20001, 'insert fail tipo fator producao inválido');
-WHEN OTHERS THEN
+    WHEN OTHERS THEN
         DBMS_OUTPUT.PUT_LINE('Erro: ' || SQLCODE || ' - ' || SQLERRM);
+        DBMS_OUTPUT.PUT_LINE('Inserção mal-sucedida devido a um erro inesperado.');
 END;
+/
 
 
-
---FUNÇÃO VALIDAÇÃO FATOR PRODUCAO
 
 CREATE OR REPLACE FUNCTION validateFatorProducao(fp fatorproducao.designacao%type) RETURN BOOLEAN
 IS
-v_boolean boolean;
-v_count int;
+    v_count INT;
 BEGIN
+SELECT COUNT(*) INTO v_count
+FROM fatorProducao
+WHERE fatorProducao.designacao = fp;
 
-select count(*) into v_count
-from fatorProducao
-where fatorProducao.designacao = fp;
-
-if v_count = 0 then
-return false;
-else
-return true;
-end if;
+IF v_count = 0 THEN
+        RAISE_APPLICATION_ERROR(-20001, 'Fator de produção inválido');
+ELSE
+        RETURN TRUE;
+END IF;
 END;
+/
 
 
---FUNÇÃO VALIDAÇÃO OPERAÇÃO
-
-
+-- Função para validar tipo de operação
 CREATE OR REPLACE FUNCTION validateOperacao(op tipoOperacao.id%type) RETURN BOOLEAN
 IS
-v_boolean boolean;
-v_count int;
+    v_count INT;
 BEGIN
+SELECT COUNT(*) INTO v_count
+FROM tipoOperacao
+WHERE tipoOperacao.id = op;
 
-select count(*) into v_count
-from tipoOperacao
-where tipoOperacao.id = op;
-
-if v_count = 0 then
-return false;
-else
-return true;
-end if;
-END;
+IF v_count = 0 THEN
+        RAISE_APPLICATION_ERROR(-20001, 'Operação inválida');
+ELSE
+        RETURN TRUE;
+END IF;
+END validateOperacao;
+/
