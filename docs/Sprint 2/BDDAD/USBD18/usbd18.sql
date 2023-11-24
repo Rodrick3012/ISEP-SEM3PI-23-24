475665cf64f7260ca52b24d40cd99db620619035
Rodrigo <1221083@isep.ipp.pt>
2023-11-24 22:11:21 +0000

    USBD18 test for demonstration inserted in script

--- docs/Sprint 2/BDDAD/USBD18/usbd18.sql
+++ docs/Sprint 2/BDDAD/USBD18/usbd18.sql
@@ -1,98 +1,73 @@
 create or replace function fncOperacoesRealizadas
-    (parcelaDada operacao.parcela%type, dataInicio operacao.data%type, 
-        dataFim operacao.data%type) return sys_refcursor
-is
+(parcelaDada operacao.parcela%type, dataAtual operacao.data%type, dataInicio operacao.data%type,
+ dataFim operacao.data%type) return sys_refcursor
+    is
     listOperacoes sys_refcursor;
     validacao boolean;
     dataInvalida exception;
 begin
-    validacao := verificardatanaoestanofuturo(datainicio);
-    validacao := verificardatanaoestanofuturo(dataInicio, dataFim);
+    validacao := fncverificardatanaoestanofuturo(dataAtual, dataInicio, dataFim);
     if validacao then
         open listOperacoes for
-            select operacao.id, operacao.parcela, tipoOperacao.tipoOperacao, 
-                operacao.data, operacao.quantidade
-            from operacao
-                inner join parcela on parcela.designacao = operacao.parcela
-                inner join tipooperacao on tipooperacao.id = operacao.tipooperacao
-            where operacao.parcela like parcelaDada
-                and operacao.data between dataInicio and dataFim
-    
-            union 
-    
-            select operacaoFatorProducao.id, operacaoFatorProducao.parcela, tipoOperacao.tipoOperacao,
-                operacaoFatorProducao.data, operacaoFatorProducao.quantidade
-            from operacaoFatorProducao
-                inner join parcela on parcela.designacao = operacaoFatorProducao.parcela
-                inner join fatorProducao on fatorproducao.designacao = operacaofatorproducao.fatorproducao
-                inner join tipoOperacao on tipoOperacao.id = operacaoFatorProducao.tipooperacao
-            where operacaoFatorProducao.parcela like parcelaDada
-                and operacaoFatorProducao.data between dataInicio and dataFim;
+            select data, nvl(cultura, 'Inexistente') cultura, parcela, tipoOperacao
+            from (select operacao.data,
+                         operacao.parcela,
+                         cultura.planta cultura,
+                         tipoOperacao.tipooperacao
+                  from operacao
+                           inner join parcela on parcela.designacao = operacao.parcela
+                           inner join culturaoperacao co on co.operacao = operacao.id
+                           inner join cultura on cultura.id = co.cultura
+                           inner join tipoOperacao on tipoOperacao.id = operacao.tipoOperacao
+
+                  union all
+
+                  select operacaoFatorProducao.data,
+                         operacaoFatorProducao.parcela,
+                         cultura.planta cultura,
+                         tipoOperacao.tipoOperacao
+                  from operacaoFatorProducao
+                           inner join parcela on parcela.designacao = operacaoFatorProducao.parcela
+                           inner join culturaoperacaofatorproducao cofp on cofp.operacaofatorproducao = operacaofatorproducao.id
+                           inner join cultura on cultura.id = cofp.cultura
+                           inner join tipoOperacao on tipoOperacao.id = operacaoFatorProducao.tipoOperacao
+                           
+                  union all
+
+                  select cast(operacaoRega.horario as date) data,
+                         operacaoRega.parcela,
+                         cultura.planta cultura,
+                         'Rega'
+                  from operacaoRega
+                           inner join parcela on parcela.designacao = operacaoRega.parcela
+                           inner join cultura on cultura.id = operacaorega.cultura)
+            where parcela like parcelaDada
+              and data between dataInicio and dataFim;
         return (listOperacoes);
     else
         raise dataInvalida;
     end if;
-    exception
-        when dataInvalida then
-            raise_application_error(-20001, 'Data invï¿½lida');
-end;
-
-
-
-
---blocos anonimos--
-        set serveroutput on;
-
--- Teste com data de inicio correta e intervalo de tempo correto
-declare
-listOperacoes sys_refcursor;
-    idOperacao operacao.id%type;
-    parcelaOperacao operacao.parcela%type;
-    tipo tipoOperacao.tipoOperacao%type;
-    dataOperacao operacao.data%type;
-    quantidadeOperacao operacao.quantidade%type;
-begin
-    listOperacoes := getOperacoesRealizadas ('Campo grande', to_date('2010-01-01', 'YYYY-MM-DD'), to_date('2023-12-31', 'YYYY-MM-DD'));
-    loop
-fetch listOperacoes into idOperacao, parcelaOperacao, tipo, dataOperacao, quantidadeOperacao;
-        exit when listOperacoes%NOTFOUND;
-        dbms_output.put_line ('ID: '|| idOperacao ||' Parcela: '|| parcelaOperacao ||' Tipo: '|| tipo || ' Data: '|| dataOperacao ||' Quantidade: '||quantidadeOperacao);
-end loop;
+exception
+    when dataInvalida then
+        raise_application_error(-20001, 'Data inválida');
 end;
 /
 
--- Teste com data de inicio incorreta (no futuro)
+-- Bloco anónimo para demonstração
 declare
-listOperacoes sys_refcursor;
-    idOperacao operacao.id%type;
-    parcelaOperacao operacao.parcela%type;
-    tipo tipoOperacao.tipoOperacao%type;
+    listOperacoes sys_refcursor;
     dataOperacao operacao.data%type;
-    quantidadeOperacao operacao.quantidade%type;
+    culturaOperacao cultura.planta%type;
+    var_tipoOperacao tipoOperacao.tipoOperacao%type;
+    parcelaOperacao operacao.parcela%type := 'Campo novo';
+    dataAtual operacao.data%type := to_date('24/11/2023', 'DD/MM/YYYY');
+    dataInicio operacao.data%type := to_date('01/07/2023', 'DD/MM/YYYY');
+    dataFim operacao.data%type := to_date('02/10/2023', 'DD/MM/YYYY');
 begin
-    listOperacoes := getOperacoesRealizadas ('Campo grande', to_date('2030-01-01', 'YYYY-MM-DD'),  to_date('2023-12-31', 'YYYY-MM-DD'));
+    listOperacoes := fncoperacoesrealizadas(parcelaOperacao, dataAtual, dataInicio, dataFim);
     loop
-fetch listOperacoes into idOperacao, parcelaOperacao, tipo, dataOperacao, quantidadeOperacao;
+        fetch listOperacoes into dataOperacao, culturaOperacao, parcelaOperacao, var_tipoOperacao;
         exit when listOperacoes%NOTFOUND;
-        dbms_output.put_line ('ID: '|| idOperacao ||' Parcela: '|| parcelaOperacao ||' Tipo: '|| tipo || ' Data: '|| dataOperacao ||' Quantidade: '||quantidadeOperacao);
-end loop;
+        dbms_output.put_line(var_tipoOperacao || ' ' || to_char(dataOperacao, 'DD/MM/YYYY') || ' ' || culturaOperacao);
+    end loop;
 end;
-/
-
--- Teste com data de inicio correta, mas com intervalo de tempo incorreto (data inicial maior que a final)
-declare
-listOperacoes sys_refcursor;
-    idOperacao operacao.id%type;
-    parcelaOperacao operacao.parcela%type;
-    tipo tipoOperacao.tipoOperacao%type;
-    dataOperacao operacao.data%type;
-    quantidadeOperacao operacao.quantidade%type;
-begin
-    listOperacoes := getOperacoesRealizadas ('Campo grande', to_date('2020-01-01', 'YYYY-MM-DD'),  to_date('2018-12-31', 'YYYY-MM-DD'));
-    loop
-fetch listOperacoes into idOperacao, parcelaOperacao, tipo, dataOperacao, quantidadeOperacao;
-        exit when listOperacoes%NOTFOUND;
-        dbms_output.put_line ('ID: '|| idOperacao ||' Parcela: '|| parcelaOperacao ||' Tipo: '|| tipo || ' Data: '|| dataOperacao ||' Quantidade: '||quantidadeOperacao);
-end loop;
-end;
-/
