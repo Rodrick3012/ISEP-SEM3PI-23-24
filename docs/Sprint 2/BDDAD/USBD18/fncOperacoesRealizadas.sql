b44648e6a6ff71f56ad69e3818b71048f0481bcd
Rodrigo <1221083@isep.ipp.pt>
2023-11-26 15:49:43 +0000

    Script fixed and done for demonstration

--- docs/Sprint 2/BDDAD/USBD18/fncOperacoesRealizadas.sql
+++ docs/Sprint 2/BDDAD/USBD18/fncOperacoesRealizadas.sql
@@ -9,15 +9,15 @@ begin
     validacao := fncverificardatanaoestanofuturo(dataAtual, dataInicio, dataFim);
     if validacao then
         open listOperacoes for
-            select data, nvl(cultura, 'Inexistente') cultura, parcela, tipoOperacao
+            select data, nvl(cultura, ' ') cultura, parcela, tipoOperacao
             from (select operacao.data,
                          operacao.parcela,
                          cultura.planta cultura,
                          tipoOperacao.tipooperacao
                   from operacao
                            inner join parcela on parcela.designacao = operacao.parcela
-                           inner join culturaoperacao co on co.operacao = operacao.id
-                           inner join cultura on cultura.id = co.cultura
+                           left join culturaoperacao co on co.operacao = operacao.id
+                           left join cultura on cultura.id = co.cultura
                            inner join tipoOperacao on tipoOperacao.id = operacao.tipoOperacao
 
                   union all
@@ -28,10 +28,10 @@ begin
                          tipoOperacao.tipoOperacao
                   from operacaoFatorProducao
                            inner join parcela on parcela.designacao = operacaoFatorProducao.parcela
-                           inner join culturaoperacaofatorproducao cofp on cofp.operacaofatorproducao = operacaofatorproducao.id
-                           inner join cultura on cultura.id = cofp.cultura
+                           left join culturaoperacaofatorproducao cofp on cofp.operacaofatorproducao = operacaofatorproducao.id
+                           left join cultura on cultura.id = cofp.cultura
                            inner join tipoOperacao on tipoOperacao.id = operacaoFatorProducao.tipoOperacao
-                           
+
                   union all
 
                   select cast(operacaoRega.horario as date) data,
@@ -40,27 +40,31 @@ begin
                          'Rega'
                   from operacaoRega
                            inner join parcela on parcela.designacao = operacaoRega.parcela
-                           inner join cultura on cultura.id = operacaorega.cultura)
+                           left join cultura on cultura.id = operacaorega.cultura)
             where parcela like parcelaDada
-              and data between dataInicio and dataFim;
+              and data between dataInicio and dataFim
+            group by tipoOperacao, data, parcela, cultura
+            order by data, tipoOperacao;
         return (listOperacoes);
     else
         raise dataInvalida;
     end if;
 exception
     when dataInvalida then
-        raise_application_error(-20001, 'Data inv�lida');
+        raise_application_error(-20001, 'Data inválida');
 end;
 
 
 -- Bloco an�nimo para demonstra��o
+set serveroutput on;
+
 declare
     listOperacoes sys_refcursor;
     dataOperacao operacao.data%type;
     culturaOperacao cultura.planta%type;
     var_tipoOperacao tipoOperacao.tipoOperacao%type;
     parcelaOperacao operacao.parcela%type := 'Campo novo';
-    dataAtual operacao.data%type := to_date('24/11/2023', 'DD/MM/YYYY');
+    dataAtual operacao.data%type := to_date('29/11/2023', 'DD/MM/YYYY');
     dataInicio operacao.data%type := to_date('01/07/2023', 'DD/MM/YYYY');
     dataFim operacao.data%type := to_date('02/10/2023', 'DD/MM/YYYY');
 begin
@@ -68,6 +72,6 @@ begin
     loop
         fetch listOperacoes into dataOperacao, culturaOperacao, parcelaOperacao, var_tipoOperacao;
         exit when listOperacoes%NOTFOUND;
-        dbms_output.put_line(to_char(dataOperacao, 'DD/MM/YYYY') || ' operação de ' || var_tipoOperacao || ' ' || culturaOperacao);
+        dbms_output.put_line(to_char(dataOperacao, 'DD/MM/YYYY') || ' operação de ' || var_tipoOperacao || ', ' || culturaOperacao);
     end loop;
 end;
