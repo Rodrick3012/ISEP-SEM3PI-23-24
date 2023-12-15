35338c7d0b4a90492780df0eaee02d89d6409a0a
Rodrigo <1221083@isep.ipp.pt>
2023-12-15 16:08:44 +0000

    L22G-201 #time 30m Blocos anónimos criados para teste de ativação do trigger

--- docs/Sprint 3/BDDAD/USBD27/usbd27.sql
+++ docs/Sprint 3/BDDAD/USBD27/usbd27.sql
@@ -9,3 +9,15 @@ exception
     when proibidoAlterarApagarLogs then
         raise_application_error(-20001, 'Não é possível alterar ou apagar as logs');
 end;
+/
+
+-- Bloco anónimo para teste da ativação do trigger numa alteração
+
+update logoperacoes
+set tipoalteracao = 2
+where idoperacao = 23;
+
+-- Bloco anónimo para teste da ativação do trigger quando apaga um registo
+
+delete from logoperacoes
+where idoperacao = 23;
