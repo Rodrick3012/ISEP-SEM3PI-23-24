3066187421a0241f7ed75edfa09b944d3336b5cd
Rodrigo <1220636@isep.ipp.pt>
2023-12-20 00:05:18 +0000

    fixing error in moving median

--- arqcp_sensors/sprint3/ProcessadorDeDados/algorithmUs11.c
+++ arqcp_sensors/sprint3/ProcessadorDeDados/algorithmUs11.c
@@ -8,7 +8,7 @@
 
 
 void algoritmoUs11(Sensor **arraySensor, int numSensores, int d) {
-    const char *nomeDoArquivo = "readSensor/infoSensores.txt";
+    const char *nomeDoArquivo = "infoSensores.txt";
     const int intervaloSegundos = 6;  
     char serialize[256];
     char * ptrSerialize = serialize;    
@@ -30,36 +30,35 @@ void algoritmoUs11(Sensor **arraySensor, int numSensores, int d) {
 				Sensor* sensor = arraySensor[(*output) - 1];
                 insertSensorData(linha, sensor);
 
-                // Obtém o tempo atual
-				time_t currentTime;
-				time(&currentTime);
+                extract_token(linha,"#time:",output);
 
+/*
 				// Verifica se o sensor ultrapassou o timeout
-				if (difftime(currentTime, sensor->last_received_time) > sensor->timeout) {
+				if ( (*output) - sensor->last_received_time  > sensor->timeout) {
 				printf("Alerta: Sensor %d ultrapassou o timeout!\n", sensor->sensor_id);
 				sensor->isInError = 1;
-			    contadorLeitura++;
 				}
+			*/	
+				contadorLeitura++;
+           } 
                 FILE* fileSerialize = fopen("serialize.txt", "w"); // Abre o ficheiro para escrita 
                 if (fileSerialize != NULL) {
 					for (int i = 0; i < numSensores; i++) {
 						calculateMovingMedian(arraySensor[i]);
 						char* sensorString = buildSensorString(arraySensor[i]);
 						fprintf(fileSerialize,"%s\n", sensorString);
-						printArray(arraySensor[i]->mediana,arraySensor[i]->elementosMediana);
+						printMovingMedianArray(arraySensor[i]->mediana,arraySensor[i]->elementosMediana);
 						free(sensorString);
 					}		
                 fclose(fileSerialize); // Fechar o ficheiro
                }else {
 				fprintf(stderr, "Erro ao abrir o arquivo para escrita\n");
 				break;
-			   }
-		  sleep(intervaloSegundos);	   
-        }
+		        }
         } else {
             fprintf(stderr, "Erro ao abrir o arquivo.\n");
         }
-       
+	break;	
    
 	}
 	return 0;
--- arqcp_sensors/sprint3/ProcessadorDeDados/readSensor/main.c
+++ arqcp_sensors/sprint3/ProcessadorDeDados/readSensor/main.c
@@ -7,15 +7,13 @@
 #include <termios.h>
 #include <signal.h>
 #include <time.h>
+#include <poll.h>
 
-//quantidade de linhas do ficheiro que vai ler por cada ciclo do algoritmo
-int d = 10;
 
 volatile sig_atomic_t done = 0;
 
 
-
-void saveSerialDataToFile(const char *portName, const char *fileName) {
+void saveSerialDataToFile(const char *portName, const char *fileName, int numValuesToRead) {
     int serial = open(portName, O_RDONLY | O_NOCTTY | O_NDELAY);
 
     if (serial == -1) {
@@ -47,22 +45,24 @@ void saveSerialDataToFile(const char *portName, const char *fileName) {
     char buffer[1024];
     ssize_t bytesRead;
     size_t bufferPos = 0;
+    
+    int valuesRead = 0;
 
-    time_t start_time = time(NULL);
-    while (!done && (time(NULL) - start_time) <= 2000) { // Read for 20 seconds
+    while (!done && valuesRead < numValuesToRead) {
         bytesRead = read(serial, buffer + bufferPos, sizeof(buffer) - bufferPos);
         if (bytesRead > 0) {
             bufferPos += bytesRead;
 
             // Check if a newline character is present in the buffer
             char *newlinePos;
-            while ((newlinePos = memchr(buffer, '\n', bufferPos)) != NULL) {
+            while ((newlinePos = memchr(buffer, '\n', bufferPos)) != NULL && valuesRead < numValuesToRead) {
                 size_t lineLength = newlinePos - buffer + 1;
 
                 // Check if the character count in the line is greater than 80
                 if (lineLength > 65) {
                     fwrite(buffer, 1, lineLength, file);
                     fflush(file);
+                    valuesRead++;  // Increment the count of values read
                 }
 
                 // Shift the remaining data in the buffer
@@ -72,15 +72,17 @@ void saveSerialDataToFile(const char *portName, const char *fileName) {
         }
     }
 
+    printf("Dados do sensor recebidos no txt\n");
     fclose(file);
     close(serial);
 }
 
-int main(int argc, char *argv[]) {
-	const char *serialPort = "/dev/ttyACM0";  // Change this to your serial port on macOS
+int main() {
+	const char *serialPort = "/dev/ttyS0";  // Change this to your serial port on macOS
     const char *outputFile = "infoSensores.txt";
 
-	saveSerialDataToFile(serialPort,outputFile);
-	return 0;
-}
+    saveSerialDataToFile(serialPort, outputFile, 10);
+    saveSerialDataToFile(serialPort, outputFile, 10);
 
+    return 0;
+}
