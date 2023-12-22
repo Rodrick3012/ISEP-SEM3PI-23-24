61e923ca01610492bbdc0eeab7917fffed35a52b
Rodrigo <1221083@isep.ipp.pt>
2023-12-22 17:09:39 +0000

    L22G-212 #time 7h USEI09 tentativa de implementação (pouco otimizado)

--- src/src/main/java/sem3pl/dei/isep/ipp/pt/esinf/application/implementation/sprint3/USEI09.java
+++ src/src/main/java/sem3pl/dei/isep/ipp/pt/esinf/application/implementation/sprint3/USEI09.java
@@ -5,43 +5,57 @@ import sem3pl.dei.isep.ipp.pt.esinf.application.domain.LocationCriteria;
 import sem3pl.dei.isep.ipp.pt.esinf.application.graph.Algorithms;
 import sem3pl.dei.isep.ipp.pt.esinf.application.graph.CommonGraph;
 import sem3pl.dei.isep.ipp.pt.esinf.application.graph.Edge;
+import sem3pl.dei.isep.ipp.pt.esinf.application.graph.Graph;
 
 import java.util.*;
 import java.util.function.BinaryOperator;
 
 
 public class USEI09 {
-    public Map<Locals, Set<Locals>> organizeClusters(CommonGraph<Locals, Integer> g, Integer nClusters, ArrayList<LocationCriteria> hubs){
+    public Map<Locals, Set<Locals>> organizeClusters(Graph<Locals, Integer> g, Integer nClusters) {
         Map<Locals, Set<Locals>> hubsAndClusters = new HashMap<>();
-        ArrayList<LinkedList<Locals>> paths = new ArrayList<>();
-        ArrayList<Integer> dists = new ArrayList<>();
-        if(g != null){
-            organizeClusters(g, hubsAndClusters, Integer::compare, Integer::sum, 0, paths, dists, Integer.MAX_VALUE, nClusters, hubs);
+        if (g != null) {
+            organizeClusters(g, hubsAndClusters, Integer::compare, Integer::sum, 0, Integer.MAX_VALUE, nClusters);
         }
         return hubsAndClusters;
     }
 
-    private static <V,E> void organizeClusters(CommonGraph<V, E> g, Map<V, Set<V>> hubsAndClusters, Comparator<E> ce, BinaryOperator<E> sum, E zero, ArrayList<LinkedList<V>> paths, ArrayList<E> dists, E infinity, Integer nClusters, ArrayList<LocationCriteria> hubs){
-        while(g.numEdges() > 0 && hubsAndClusters.size() < nClusters){
-            Edge<V,E> edgeToRemove = findEdgeWithMaxShortestPaths(g, ce, sum, zero, paths, dists, infinity);
-            g.removeEdge(edgeToRemove.getVOrig(), edgeToRemove.getVDest());
+    private static <V, E> void organizeClusters(Graph<V, E> g, Map<V, Set<V>> hubsAndClusters, Comparator<E> ce, BinaryOperator<E> sum, E zero, E infinity, Integer nClusters) {
+        Set<V> visited = new HashSet<>();
 
-            List<Set<V>> connectedComponents = getConnectedComponents(g);
-            for(Set<V> component : connectedComponents){
-                V hub = findHubInCluster(component, hubs);
-                hubsAndClusters.put(hub, component);
+        while (hubsAndClusters.size() < nClusters) {
+            Edge<V, E> edgeToRemove = findEdgeWithMaxShortestPaths(g, ce, sum, zero, infinity, visited);
+            if (edgeToRemove != null) {
+                V origVertex = edgeToRemove.getVOrig();
+                V destVertex = edgeToRemove.getVDest();
+
+                g.removeEdge(origVertex, destVertex);
+
+                List<Set<V>> connectedComponents = getConnectedComponents(g);
+                if (connectedComponents.size() > hubsAndClusters.size()) {
+                    hubsAndClusters.clear();
+                    for (Set<V> component : connectedComponents) {
+                        V hub = findHubInCluster(g, component);
+                        hubsAndClusters.put(hub, component);
+                    }
+                } else {
+                    g.addEdge(origVertex, destVertex, edgeToRemove.getWeight());
+                    visited.add(origVertex);
+                    visited.add(destVertex);
+                }
+            } else {
+                break;
             }
         }
     }
 
-    private static <V,E> Edge<V,E> findEdgeWithMaxShortestPaths(CommonGraph<V,E> g, Comparator<E> ce, BinaryOperator<E> sum, E zero, ArrayList<LinkedList<V>> paths, ArrayList<E> dists, E infinity){
-
-        Edge<V,E> edgeWithMaxPaths = null;
+    private static <V, E> Edge<V, E> findEdgeWithMaxShortestPaths(Graph<V, E> g, Comparator<E> ce, BinaryOperator<E> sum, E zero, E infinity, Set<V> visited) {
+        Edge<V, E> edgeWithMaxPaths = null;
         int maxPaths = 0;
 
-        for(Edge<V,E> edge : g.edges()){
-            int numPaths = calculateShortestPaths(g, edge, ce, sum, zero, paths, dists, infinity);
-            if(numPaths > maxPaths){
+        for (Edge<V, E> edge : g.edges()) {
+            int numPaths = calculateShortestPaths(g, edge, ce, sum, zero, infinity);
+            if (numPaths > maxPaths) {
                 maxPaths = numPaths;
                 edgeWithMaxPaths = edge;
             }
@@ -49,32 +63,33 @@ public class USEI09 {
         return edgeWithMaxPaths;
     }
 
-    private static  <V,E> List<Set<V>> getConnectedComponents(CommonGraph<V,E> g){
+    private static <V, E> List<Set<V>> getConnectedComponents(Graph<V, E> g) {
         List<Set<V>> connectedComponents = new ArrayList<>();
         Set<V> visitedVertices = new HashSet<>();
 
-        for (V vertex : g.vertices()){
-            if(!visitedVertices.contains(vertex)){
+        for (V vertex : g.vertices()) {
+            if (!visitedVertices.contains(vertex)) {
                 Set<V> connectedComponent = depthFirstSearch(g, vertex);
                 connectedComponents.add(connectedComponent);
-                visitedVertices.addAll(connectedComponent);
+                if (connectedComponent != null) {
+                    visitedVertices.addAll(connectedComponent);
+                }
             }
         }
         return connectedComponents;
     }
 
-    private static <V,E> Set<V> depthFirstSearch(CommonGraph<V,E> g, V vertex){
+    private static <V, E> Set<V> depthFirstSearch(Graph<V, E> g, V vertex) {
         LinkedList<V> result = Algorithms.depthFirstSearch(g, vertex);
-        if(result != null) {
-            return new HashSet<>(result);
-        } else return new HashSet<>();
+        return (result != null) ? new HashSet<>(result) : null;
     }
 
-
-    private static <V,E> int calculateShortestPaths(CommonGraph<V, E> g, Edge<V, E> edge, Comparator<E> ce, BinaryOperator<E> sum, E zero, ArrayList<LinkedList<V>> paths, ArrayList<E> dists, E infinity) {
+    private static <V, E> int calculateShortestPaths(Graph<V, E> g, Edge<V, E> edge, Comparator<E> ce, BinaryOperator<E> sum, E zero, E infinity) {
         V sourceVertex = edge.getVOrig();
         V targetVertex = edge.getVDest();
 
+        ArrayList<LinkedList<V>> paths = new ArrayList<>();
+        ArrayList<E> dists = new ArrayList<>();
         boolean success = Algorithms.shortestPaths(g, sourceVertex, ce, sum, zero, paths, dists, infinity);
 
         if (!success) {
@@ -91,20 +106,10 @@ public class USEI09 {
         return countPaths;
     }
 
-    @SuppressWarnings("unchecked")
-    private static <V,E> V findHubInCluster(Set<V> cluster, ArrayList<LocationCriteria> hubs) {
-        V selectedHub = null;
-
-        for(LocationCriteria criteria : hubs){
-            V hub = (V) criteria.getLocation();
-
-            if(cluster.contains(hub)){
-                if(selectedHub == null){
-                    selectedHub = hub;
-                } else return null;
-            }
+    private static <V, E> V findHubInCluster(Graph<V, E> g, Set<V> cluster) {
+        if (g == null || cluster == null || cluster.isEmpty()) {
+            return null;
         }
-
-        return selectedHub;
+        return cluster.stream().max(Comparator.comparingInt(v -> g.inDegree(v) + g.outDegree(v))).orElse(null);
     }
 }
