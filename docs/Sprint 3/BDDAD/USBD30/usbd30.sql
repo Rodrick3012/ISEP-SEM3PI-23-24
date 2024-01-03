520b665352534184a50a95b90b715f8c3b876c76
rodri <1220636@isep.ipp.pt>
2024-01-03 15:52:49 +0000

    fixing us 30

--- docs/Sprint 3/BDDAD/USBD30/usbd30.sql
+++ docs/Sprint 3/BDDAD/USBD30/usbd30.sql
@@ -3,52 +3,35 @@ create or replace NONEDITIONABLE PROCEDURE pcd_anular_operacao_prevista (idOpera
     v_data_operacao date;
     v_diferenca_dias number;
     v_id_operacao number := -1;
-
+    v_result BOOLEAN;
     ex_data_maior_que_tres_dias EXCEPTION;
     ex_operacoes_dependentes EXCEPTION;
 
 
-
 BEGIN
 
-select id into v_id_operacao
-from(
-        select id from semeadura
-        union
-        select id from plantacao
-        union
-        select id from colheita
-    ) where id = idOperacao;
+    v_result := verifica_id(v_id_operacao);
 
-
--- Verifica se o v_id_operacao foi alterado
-IF v_id_operacao = -1 THEN
-
+    IF  v_result THEN
+    RAISE ex_operacoes_dependentes;
+ELSE
 select data into v_data_operacao
 from operacao
 where operacao.id = idOperacao;
 
--- Calcule a diferença em dias entre a data atual e a data da operação
 v_diferenca_dias := SYSDATE - v_data_operacao;
 
-        -- Verifique se a diferença é menor que 3 dias
         IF v_diferenca_dias < 3 THEN
-        -- Atualize o estado da operação para anulada
 UPDATE Operacao SET anulada = 1
 WHERE operacao.id = idOperacao;
 
 DBMS_OUTPUT.PUT_LINE('Operação anulada com sucesso.');
 ELSE
-
-        RAISE ex_data_maior_que_tres_dias;
+        raise ex_data_maior_que_tres_dias;
 END IF;
 
-ELSE
-
-    RAISE ex_operacoes_dependentes;
 
 END IF;
-
 
 EXCEPTION
     WHEN NO_DATA_FOUND THEN
@@ -64,3 +47,43 @@ WHEN OTHERS THEN
         RAISE_APPLICATION_ERROR(-20001, 'Erro: ' || SQLCODE || ' - ' || SQLERRM);
         RAISE;
 END pcd_anular_operacao_prevista;
+
+
+
+
+
+
+
+CREATE OR REPLACE FUNCTION verifica_id(idOperacao INT) RETURN BOOLEAN AS
+   v_contador INT;
+BEGIN
+   -- Tenta encontrar o ID da operação
+SELECT COUNT(id)
+INTO v_contador
+FROM (
+         SELECT id FROM semeadura
+         UNION
+         SELECT id FROM plantacao
+         UNION
+         SELECT id FROM colheita
+     )
+WHERE id = idOperacao;
+
+-- Se v_contador for maior que zero, o ID foi encontrado
+RETURN v_contador > 0;
+END;
+
+
+DECLARE
+v_idOperacao INT := 10;
+   v_result BOOLEAN;
+BEGIN
+   v_result := verifica_id(v_idOperacao);
+
+   IF v_result THEN
+      DBMS_OUTPUT.PUT_LINE('ID ' || v_idOperacao || ' encontrado.');
+ELSE
+      DBMS_OUTPUT.PUT_LINE('ID ' || v_idOperacao || ' não encontrado.');
+END IF;
+END;
+/
